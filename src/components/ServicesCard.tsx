e96c523d5aac27b5d5e64b29c24d92a425f814f8
PolywebDigital <polyweb.digital@gmail.com>
2024-03-25 20:32:11 +0100

    fix: remove know more on services card

--- src/components/ServicesCard.tsx
+++ src/components/ServicesCard.tsx
@@ -1,6 +1,3 @@
-import { GoArrowRight } from "react-icons/go";
-import { Button } from "../components/ui/button";
-
 interface ServicesCardProps {
   title: string;
   imageUrl: string;
@@ -16,9 +13,6 @@ const ServicesCard: React.FC<ServicesCardProps> = (props) => {
       <div className="flex flex-col gap-6 grow text-center sm:text-left justify-between items-center lg:items-start">
         <h3 className="text-3xl font-semibold"> {title} </h3>
         <p> {content} </p>
-        <Button buttonType="bordered">
-          En savoir plus <GoArrowRight />
-        </Button>
       </div>
     </div>
   );
